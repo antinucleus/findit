caeee3b752acd8dec1f70a81607fa18c3bd95dc9
antinucleus <antinucleuss@gmail.com>
2023-01-25 11:54:18 +0300

    feat: Add @solana/web3.js

--- src/features/auth/routes/Register.tsx
+++ src/features/auth/routes/Register.tsx
@@ -1,10 +1,33 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {View, Text, Button} from 'react-native';
+import {Keypair, Connection, clusterApiUrl} from '@solana/web3.js';
 
 export const Register = () => {
+  const [keypair, setKeypair] = useState<Keypair>(() => Keypair.generate());
+  const randomKeypair = () => {
+    setKeypair(() => Keypair.generate());
+  };
+
+  const [version, setVersion] = useState<any>('');
+  useEffect(() => {
+    const conn = new Connection(clusterApiUrl('devnet'));
+    conn.getVersion().then(r => {
+      setVersion(r);
+    });
+  }, []);
   return (
     <View>
-      <Text>Register</Text>
+      {version ? (
+        <View>
+          <Text>{JSON.stringify(version, null, 2)}</Text>
+        </View>
+      ) : null}
+      {keypair ? (
+        <View>
+          <Text>{JSON.stringify(keypair?.publicKey?.toBase58(), null, 2)}</Text>
+        </View>
+      ) : null}
+      <Button title="New Keypair" onPress={randomKeypair} />
     </View>
   );
 };
